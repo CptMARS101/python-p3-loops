76ab09fb21dd4d5b162982bfd1234a0d33911aed
Marcus Kitchen <marcusdkitchen@gmail.com>
2024-01-09 10:35:39 -0500

    loopsLAB

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    n = 10
+    while n > 0:
+        print(n)
+        n -= 1
+        print("Happy New Year!")
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    sq_list = []
+    for num in int_list:
+        sq = num * num
+        sq_list.append(sq)
+    return sq_list
+        
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
